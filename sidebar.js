3d8289d59b92e6ae6d51476184140347beaa26e8
gtkrprivateserver <gtkrprivateserver@gmail.com>
2026-02-16 01:36:54 +0700

    sidebar.js 만들기

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+<div class="sidebar">
+  <a href="index.html">Home</a>
+  <a href="about.html">About</a>
+  <a href="rules.html">Rules</a>
+  <a href="events.html">Events</a>
+  <a href="join.html">Join</a>
+</div>
