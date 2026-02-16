0deeba1ea9f6577579a3abff8ce1e3e8fcd8b031
gtkrprivateserver <gtkrprivateserver@gmail.com>
2026-02-16 07:21:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Contoh interaksi tombol beli tiket
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.querySelector(".btn");
+
+  btn.addEventListener("click", function () {
+    alert("Redirecting to ticket purchase page...");
+  });
+});
