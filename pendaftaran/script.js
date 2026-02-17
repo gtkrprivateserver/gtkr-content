104093a08f02e5ae2a0186d34424ab2e5f67fdb5
gtkrprivateserver <gtkrprivateserver@gmail.com>
2026-02-18 06:38:18 +0700

    script.js 만들기

---
+++ pendaftaran/script.js
@@ -0,0 +1,29 @@
+document.getElementById("joinForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nama = document.getElementById("nama").value;
+  const walla = document.getElementById("walla").value;
+const askot = document.getElementById("askot").value;
+const role = document.getElementById("role").value;
+const status = document.getElementById("status").value;
+
+  const nomorWA = "6288269017561"; // GANTI dengan nomor WhatsApp kamu
+
+  const pesan = `Halo Admin Chaste Dragon 🐲
+Saya ingin mendaftar:
+
+Nama: ${nama}
+ID/Username walla: ${walla}
+Asal Kota: ${Askot}
+Role: ${role}
+Status: ${status}
+
+Terima kasih!
+
+Join Our Channel WhatsApp
+https://whatsapp.com/channel/0029VbBzqwoISTkNPsElaJ0m ';
+
+  const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
+
+  window.open(url, "_blank");
+});
